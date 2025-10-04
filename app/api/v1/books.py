c19f9ed1479045dc28f4548b776dd4ab2ed73ddf
Darkelvar <darkelvar@gmail.com>
2025-10-04 21:31:38 +0200

    Add book_exists error handler and status_codes

--- app/api/v1/books.py
+++ app/api/v1/books.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.session import get_session
@@ -10,17 +11,22 @@ from app.services import books
 router = APIRouter(prefix="/books", tags=["books"])
 
 
-@router.post("", response_model=BookOut)
+@router.post("", response_model=BookOut, status_code=200)
 async def create_book(book_in: BookCreate, db: AsyncSession = Depends(get_session)):
-    return await books.create_book(db, book_in)
+    try:
+        return await books.create_book(db, book_in)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=409, detail="Book with this serial number already exists."
+        )
 
 
-@router.get("", response_model=List[BookOut])
+@router.get("", response_model=List[BookOut], status_code=200)
 async def list_books(db: AsyncSession = Depends(get_session)):
     return await books.get_books(db)
 
 
-@router.get("/{serial_number}", response_model=BookOut)
+@router.get("/{serial_number}", response_model=BookOut, status_code=200)
 async def get_book(serial_number: str, db: AsyncSession = Depends(get_session)):
     book = await books.get_book(db, serial_number)
     if not book:
@@ -28,7 +34,7 @@ async def get_book(serial_number: str, db: AsyncSession = Depends(get_session)):
     return book
 
 
-@router.put("/{serial_number}", response_model=BookOut)
+@router.put("/{serial_number}", response_model=BookOut, status_code=200)
 async def update_book(
     serial_number: str, book_in: BookUpdate, db: AsyncSession = Depends(get_session)
 ):
@@ -38,9 +44,9 @@ async def update_book(
     return book
 
 
-@router.delete("/{serial_number}")
+@router.delete("/{serial_number}", status_code=204)
 async def delete_book(serial_number: str, db: AsyncSession = Depends(get_session)):
     success = await books.delete_book(db, serial_number)
     if not success:
         raise HTTPException(status_code=404, detail="Book not found")
-    return Response(status_code=204)
+    return None
